ee3fcc0818b4358906f6c9dcc937d240a2948923
Maryann Agofure <maryannagofure@gmail.com>
2021-05-30 21:35:45 +0100

    builds website

--- main.js
+++ main.js
@@ -24,5 +24,21 @@ async function Typewriter(sentence, eleRef, typespeed = 100){
 function waitForMs(ms){
     return new Promise(resolve => setTimeout(resolve,ms))
 }
-
 Typewriter('A frontend developer and content strategist', '#text-type' )
+
+async function deleteSentence(eleRef) {
+    const sentence = $(eleRef).html();
+    const letters = sentence.split("");
+    let i = 0;
+    while(letters.length > 0) {
+      await waitForMs(100);
+      letters.pop();
+      $(eleRef).html(letters.join(""));
+    }
+  }
+  $( document ).ready(async function() {
+    await Typewriter('A frontend developer and content strategist', "#text-type");
+    await waitForMs(2000);
+    deleteSentence("#text-type");
+  });
+  deleteSentence("#text-type");
